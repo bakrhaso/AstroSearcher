f63f735ee96296f69c2f3938fd6479abd8bd972a
BKHS <bardsore@gmail.com>
2023-12-08 14:05:23 +0100

    minor style change, added function to make it easier to debug finding markets

--- src/lib/saveReader.js
+++ src/lib/saveReader.js
@@ -9,10 +9,39 @@ export let character
  * @param {File} saveFile
  */
 export async function readSaveFile(saveFile) {
+	const parserOptions = {
+		ignoreAttributes: false,
+		attributesGroupName: "attr__",
+	}
+	const parser = new XMLParser(parserOptions)
+
 	/**
 	 * @type {{ CampaignEngine: CampaignEngine }}
 	 */
-	const save = new XMLParser().parse(await saveFile.text())
+	const save = parser.parse(await saveFile.text())
+	findVal(save.CampaignEngine.hyperspace.o.saved.LocationToken, 'market')
+	console.log([...paths].sort())
 	console.log(save)
 	character = save.CampaignEngine.characterData
+}
+
+let paths = new Set()
+
+function findVal(object, key, path) {
+	let value
+	Object.keys(object).forEach(k => {
+		if (k === key) {
+			value = object[k]
+			paths.add(path)
+			return true
+		}
+
+		if (object[k] && typeof object[k] === "object") {
+			const pathToUse = !isNaN(Number(k)) ? "[index]" : k
+			const locPath = path != null ? `${path}.${pathToUse}` : pathToUse
+			value = findVal(object[k], key, locPath)
+			return value != null
+		}
+	})
+	return value
 }
