eaf570e618ae39212cf017dc9c9215487dd04c35
BKHS <bardsore@gmail.com>
2023-12-08 20:36:11 +0100

    code to get markets

--- src/lib/saveReader.js
+++ src/lib/saveReader.js
@@ -12,6 +12,7 @@ export async function readSaveFile(saveFile) {
 	const parserOptions = {
 		ignoreAttributes: false,
 		attributesGroupName: "attr__",
+		attributeNamePrefix: "",
 	}
 	const parser = new XMLParser(parserOptions)
 
@@ -19,25 +20,74 @@ export async function readSaveFile(saveFile) {
 	 * @type {{ CampaignEngine: CampaignEngine }}
 	 */
 	const save = parser.parse(await saveFile.text())
-	findVal(save.CampaignEngine.hyperspace.o.saved.LocationToken, 'market')
-	console.log([...paths].sort())
+
+	// used to recursively find where a object might be within the save file, only used in developing/debugging
+	// findVal(save.CampaignEngine, 'market')
+
+	const markets = getMarkets(save.CampaignEngine.hyperspace.o.saved.LocationToken)
+	console.log([... new Set(markets.flatMap(it => it.cond.st).sort())])
+
+
 	console.log(save)
 	character = save.CampaignEngine.characterData
 }
 
-let paths = new Set()
+/**
+ * @param {LocationToken[]} locationTokens
+ * @return Market[]
+ */
+function getMarkets(locationTokens) {
+	return locationTokens.flatMap((lt) => {
+		/**
+		 * @type {Market[]}
+		 */
+		const markets = []
+
+		if (lt.orbit?.s?.o?.saved?.Plnt != null) {
+			if (Array.isArray(lt.orbit.s.o.saved.Plnt)) {
+				for (const p of lt.orbit.s.o.saved.Plnt) {
+					if (p.market != null) {
+						markets.push(p.market)
+					}
+				}
+			} else if (lt.orbit.s.o.saved.Plnt.market != null) {
+				markets.push(lt.orbit.s.o.saved.Plnt.market)
+			}
+		}
+
+		if (lt.orbit?.s?.con?.systems?.Sstm != null && Array.isArray(lt.orbit.s.con.systems.Sstm)) {
+			for (const sstm of lt.orbit.s.con.systems.Sstm) {
+				if (sstm.o?.saved?.Plnt != null) {
+					if (Array.isArray(sstm.o.saved.Plnt)) {
+						for (const p of sstm.o.saved.Plnt) {
+							if (p.market != null) {
+								markets.push(p.market)
+							}
+						}
+					} else if (sstm.o.saved.Plnt.market != null) {
+						markets.push(sstm.o.saved.Plnt.market)
+					}
+				}
+			}
+		}
+
+		return markets
+	})
+}
+
+let matches = []
 
 function findVal(object, key, path) {
 	let value
-	Object.keys(object).forEach(k => {
+	Object.keys(object).forEach((k) => {
 		if (k === key) {
 			value = object[k]
-			paths.add(path)
+			matches.push([value, path])
 			return true
 		}
 
 		if (object[k] && typeof object[k] === "object") {
-			const pathToUse = !isNaN(Number(k)) ? "[index]" : k
+			const pathToUse = k // !isNaN(Number(k)) ? "[index]" : k
 			const locPath = path != null ? `${path}.${pathToUse}` : pathToUse
 			value = findVal(object[k], key, locPath)
 			return value != null
