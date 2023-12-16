923f01c82f231a4b51de7bd94bdc4bc3b42ca5d8
BKHS <bardsore@gmail.com>
2023-12-16 22:09:18 +0100

    increase min-width on narrower screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,13 +15,19 @@
 		min-height: 100vh;
 	}
 
+	@media (max-width: 1200px) {
+		main {
+			min-width: 92%;
+		}
+	}
+
 	main {
 		flex: 1;
 		display: flex;
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 60%;
+		max-width: 70%;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
